cc5336ed1d845c42fba94a028cd158dd641800a2
ARSLAN ASLAM <beingfastian@gmail.com>
2023-01-12 03:52:40 +0500

    Update AppsView.vue

--- src/views/AppsView.vue
+++ src/views/AppsView.vue
@@ -1,31 +1,34 @@
-<script setup> 
-import { reactive } from 'vue'
-import  ProjectsService  from '@/components/ProjectsService.js' // use getProjects() from ProjectService to get all projects  
-let projects = reactive([])
-async function getProjects() {
-                try{
-                    const response = await ProjectsService.getProjects()
-                    console.log("MF (inside function) response: ", response)
-                    projects = await response.data.records
-                    console.log("MF: (inside function) projects: ", projects)  // WORKS
-                }catch(err){
-                    console.log(err)
-                }
-                }
-              getProjects() 
-              console.log("MF: (outside function) projects: ", projects)
-</script>
 <template>
-    <div class="apps"> 
-      <h1>Apps</h1>
-      <p>Here are some of my apps:</p>
-      <p>TEST: {{ projects }}</p>
-      <div class="projects">
-        <div v-for="project in projects" :key="project.id">
-          <h2>{{ project.name }}</h2>
-          <p>{{ project.description }}</p>
-          <p>{{ project.url }}</p>
-        </div>
+  <div class="apps">
+    <h1>Apps</h1>
+    <p>Here are some of my apps:</p>
+    <!-- <p>TEST: {{ state.projects }}</p> -->
+    <div class="projects">
+      <div v-for="project in state.projects" :key="project.id">
+        <h2>{{ project.fields.Title }}</h2>
+        <p>{{ project.fields.Excerpt }}</p>
+        <p>{{ project.fields.URL }}</p>
+      </div>
     </div>
-    </div>
-    </template>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+import ProjectsService from "@/components/ProjectsService.js"; // use getProjects() from ProjectService to get all projects
+let state = reactive(
+  {
+    projects: [],
+  }
+);
+async function getProjects() {
+  try {
+    const response = await ProjectsService.getProjects().then((response) => {
+      state.projects = response.data.records;
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+getProjects();
+</script>
